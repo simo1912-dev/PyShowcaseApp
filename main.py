370ef3e6bfa7d3f70e2b9cec89f9ab5ff60858c5
simo <simoaboulfath52@gmail.com>
2024-02-12 20:21:19 +0100

    Render the titles

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import pandas
 import streamlit as st
+import pandas as pd  # third party library to handle csv files
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -11,5 +13,26 @@ with col2:
     to gain hands on expreience in py
     '''
     st.info(content)
+
+content1 = '''
+Below you can find some of the apps I have built in Py
+'''
+st.write(content1)
+
+col3, col4 = st.columns(2)
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3: # layout management ...
+    for index, row in df[:10].iterrows(): # this enables iterating over each row
+        st.header(row["title"])
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
+
+
+
+
 
 
